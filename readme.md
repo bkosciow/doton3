0e4e7fdf10eaa95912be4a1b085012d608adf91b
Bartosz Kościów <kosci1@gmail.com>
2022-12-27 21:01:41 +0100

    icons set a room

--- readme.md
+++ readme.md
@@ -10,7 +10,10 @@ This time based on Kivy with HDMI display.
 
 ### Credits
 
-Weather icons made by [Freepik](http://www.flaticon.com/authors/freepik) and [Linector](http://www.flaticon.com/authors/linector) from [www.flaticon.com](http://www.flaticon.com)
- 
+Weather icons: [Freepik](http://www.flaticon.com/authors/freepik) and [Linector](http://www.flaticon.com/authors/linector) from [www.flaticon.com](http://www.flaticon.com)
+
+Printer icon: [3d print icons created by deemakdaksina - Flaticon](https://www.flaticon.com/free-icons/3d-print)
+
+Bulb icon: [Idea icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/idea)
  
 [https://github.com/TheCodeSummoner/kivy-circular-progress-bar](https://github.com/TheCodeSummoner/kivy-circular-progress-bar)
--- view/room.py
+++ view/room.py
@@ -1,5 +1,11 @@
 from view.label_border import LabelBorder
 from service.widget import Action
+from kivy.uix.popup import Popup
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
+from widget.relay_switch import RelaySwitch
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 
 
 class Room(LabelBorder):
@@ -10,10 +16,21 @@ class Room(LabelBorder):
                 if isinstance(obj, Action):
                     detected.append(obj)
 
-            print(detected)
             if not detected:
                 return
+
             if len(detected) == 1:
                 detected[0].action(touch)
             else:
-                print('popup')
+                content = StackLayout()
+                content.padding = [15, 25, 15, 15]
+                content.spacing = [10, 10]
+                popup = Popup(title="Actions", size_hint=(0.6, 0.6))
+                for item in detected:
+                    button = RelaySwitch(node_name=item.node_name, channel=item.channel, text=item.label)
+                    button.state = 'normal' if item.enabled == 1 else 'down'
+                    content.add_widget(button)
+                popup.content = content
+
+                popup.open()
+
